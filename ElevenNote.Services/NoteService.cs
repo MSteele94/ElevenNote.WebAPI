fd19d7156aeb20cfade1a32838d1eaedf4d78033
MSteele94 <bengalsfan620@gmail.com>
2021-04-07 15:08:47 -0400

    5th commit

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -52,6 +52,21 @@ namespace ElevenNote.Services
                     };
             }
         }
+        public bool UpdateNote(NoteEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
         public IEnumerable<NoteListItem> GetNotes()
         {
             using (var ctx = new ApplicationDbContext())
